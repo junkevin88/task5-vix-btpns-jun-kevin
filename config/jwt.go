42556876ed417fdc10d5defb1da847ed54ff787c
junkevin88 <junkevin88@gmail.com>
2023-04-22 12:21:03 +0700

    add jwt

---
+++ config/jwt.go
@@ -0,0 +1,59 @@
+package config
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"errors"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+	"strings"
+)
+
+var secretKey = generateSecretKey()
+
+func generateSecretKey() string {
+	b := make([]byte, 32) // 32 bytes = 256 bits
+	_, err := rand.Read(b)
+	if err != nil {
+		panic(err)
+	}
+	return base64.URLEncoding.EncodeToString(b)
+}
+func GenerateToken(id uint, email string) string {
+	claims := jwt.MapClaims{
+		"id":    id,
+		"email": email,
+	}
+
+	parseToken := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	signedToken, _ := parseToken.SignedString([]byte(secretKey))
+
+	return signedToken
+}
+
+func VerifyToken(c *gin.Context) (interface{}, error) {
+	errResponse := errors.New("sign in to proceed")
+	headerToken := c.Request.Header.Get("Authorization")
+	bearer := strings.HasPrefix(headerToken, "Bearer")
+
+	if !bearer {
+		return nil, errResponse
+	}
+
+	stringToken := strings.Split(headerToken, " ")[1]
+
+	token, _ := jwt.Parse(stringToken, func(t *jwt.Token) (interface{}, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errResponse
+		}
+
+		return []byte(secretKey), nil
+	})
+
+	if _, ok := token.Claims.(jwt.MapClaims); !ok && !token.Valid {
+		return nil, errResponse
+	}
+
+	return token.Claims.(jwt.MapClaims), nil
+}
